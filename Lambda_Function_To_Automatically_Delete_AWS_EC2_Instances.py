fbfc5ae9fe74c2233989aeb641f9e98ce5240c6b
Bilal-javed-96 <57318607+Bilal-javed-96@users.noreply.github.com>
2021-05-19 12:17:14 +0500

    Create Lambda_Function_To_Automatically_Delete_AWS_EC2_Instances.py
    This function will search for all instances (running & stooped) and terminate them. It can also be integrated with CloudWatch so that it can be triggered periodically or at certain events and than notification will be send through AWS SNS (Simple Notification Service).

---
+++ Lambda_Function_To_Automatically_Delete_AWS_EC2_Instances.py
@@ -0,0 +1,17 @@
+import boto3
+import json
+
+def lambda_handler(event,context):
+                                                              
+    regions=['us-east-1','us-east-2','us-west-1','us-west-2'] #regions = in regions where your instances are deployed
+    for j in regions:                                         #loop,so that code will be executed in regions defined in regions list
+        instances = []                                        #instance ids will be appended in this list
+        ec2 = boto3.client('ec2',region_name=j)
+        response = ec2.describe_instances()
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                if instance['State']['Name'] != 'terminating': #Condition for excluding terminated instances
+                    #print(instance["InstanceId"])
+                    instances.append(instance["InstanceId"])  
+                    #print(instances)
+            ec2.terminate_instances(InstanceIds=instances)    #terminating instances
